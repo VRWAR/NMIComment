0e348ad00a72d83817ee04a1909bb104b37794ad
Tim Pang <pangmain@gmail.com>
2014-05-16 15:41:01 +1000

    log configuration update for version 1.0

--- doc/creatNMICommentTable.sql
+++ doc/creatNMICommentTable.sql
@@ -1,19 +1,16 @@
-USE [VRSailor]
-GO
-
-/****** Object:  Table [dbo].[NMIComment]    Script Date: 13/05/2014 5:20:20 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
+/****** Object:  Table [dbo].[NMIComment]    Script Date: 16/05/2014 3:24:28 PM ******/
 
 CREATE TABLE [dbo].[NMIComment](
 	[id] [nvarchar](50) NOT NULL,
 	[NMI_number] [nvarchar](10) NOT NULL,
 	[comment] [nvarchar](max) NULL,
-	[comment_time] [datetime] NOT NULL
+	[comment_time] [datetime] NOT NULL,
+ CONSTRAINT [PK_NMIComment] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
 GO
+
 
